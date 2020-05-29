3bbd7d79b4567007ee07035923ffce850cf55aa0
Chirag3101 <60541317+Chirag3101@users.noreply.github.com>
2020-05-29 21:17:29 +0530

    Add files via upload

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+    $("#menu-button").click(function(){
+      $(".content").toggleClass("reduced");
+    });
+});
+
+function resize() {
+  if ($(window).width() < 990) {
+    $(".nav-item").addClass("col-6");
+  } else {
+    $(".nav-item").removeClass("col-6");
+  }
+}
+
+resize();
+$(window).on('resize', resize);
 
