4fa2a762db1f04f28eb0320d703eea9a5fa2101d
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2020-05-29 21:35:27 +0530

    Add files via upload

--- JS/navbar.js
+++ JS/navbar.js
@@ -6,14 +6,11 @@ $(document).ready(function(){
 
 function resize() {
   if ($(window).width() < 990) {
-    $(".navbar-brand img").attr('src','logobw.png');
     $(".nav-item").addClass("col-6");
   } else {
-    $(".navbar-brand img").attr('src','the elecruisers.png');
     $(".nav-item").removeClass("col-6");
   }
 }
 
 resize();
 $(window).on('resize', resize);
-// 
