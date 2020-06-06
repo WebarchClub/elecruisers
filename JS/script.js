edc4d9a4f61c5b46bee37ecc2338ea86ea6d0ed2
Chirag3101 <60541317+Chirag3101@users.noreply.github.com>
2020-06-06 15:42:31 +0530

    Delete script.js

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var burger = document.querySelector('.burger-container'),
-        header = document.querySelector('.header');
-    
-    burger.onclick = function() {
-        header.classList.toggle('menu-opened');
-        
-    }
-}());
