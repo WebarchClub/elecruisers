7cf7843ab57fd13201d5b8f5e630c744879a9db3
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2020-06-12 21:16:14 +0530

    Added styling to cars page

--- JS/cars.js
+++ JS/cars.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $(".car-more").click(function(){
         $(this).parent().next(".car-slide").slideToggle("slow");
     });
+    $(".car-less").click(function(){
+        $(this).parent().slideToggle("slow");
+    });
 });
