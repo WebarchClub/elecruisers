0991b74b52a124c033eeb38bc5ed95c2c0fd5942
Akash <gsaisriram.akash@gmail.com>
2020-06-28 15:43:15 +0530

    updated Contact-Icons

--- JS/cars.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".car-more").click(function(){
-        $(this).parent().next(".car-slide").slideToggle("slow");
-    });
-    $(".car-less").click(function(){
-        $(this).parent().slideToggle("slow");
-    });
-});
